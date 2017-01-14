ca1518e915cd610e7407530ade2a6ecadd60ea41
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2017-01-14 16:05:12 -0500

    Change excerpt length

--- functions.php
+++ functions.php
@@ -121,3 +121,9 @@ add_image_size( 'admin', '60', '60', false );
 add_image_size( 'medium_large', '1024', '0', false ); 
 add_image_size( 'small', '300', '300', false ); 
 add_image_size( 'medium-small', '500', '500', false ); 
+
+//Lower excerpt length
+function custom_excerpt_length($length) {
+    return 15;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
