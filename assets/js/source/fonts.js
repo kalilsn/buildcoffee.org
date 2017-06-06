ace1d635e61c8c8d200bb218f4478e35ec996afc
Kalil Smith-Nuevelle <ksmithnuevelle@oomphinc.com>
2017-06-06 19:43:44 -0400

    Use async webfont loader

--- assets/js/source/fonts.js
+++ assets/js/source/fonts.js
@@ -1,5 +1,12 @@
-WebFont.load({
+WebFontConfig = {
     google: {
         families: ['Lato:400,400italic,700,900']
     }
-});
+};
+
+(function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.28/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
