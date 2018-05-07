34a1772864e5314be7e5fc341d2bbe3e6ec6ea4c
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2018-05-07 11:51:17 -0500

    Add bea's website to footer

--- site/web/app/themes/buildcoffee/footer.php
+++ site/web/app/themes/buildcoffee/footer.php
@@ -9,7 +9,7 @@
 ?>
 
 <footer <?php echo is_home() ? '' : 'class="footer-alternate"'; ?>>
-	<div class="credit">This site was created by Kalil Smith-Nuevelle and Bea Malsky<a href="<?php echo wp_login_url(); ?>" tabindex="-1">.</a> Photos by <a href="http://www.alexjungtcs.com/">Alex Jung.</a></div>
+	<div class="credit">This site was created by Kalil Smith-Nuevelle and <a href="https://beamalsky.fyi/">Bea Malsky</a><a href="<?php echo wp_login_url(); ?>" tabindex="-1">.</a> Photos by <a href="http://www.alexjungtcs.com/">Alex Jung.</a></div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <?php wp_footer(); ?>
