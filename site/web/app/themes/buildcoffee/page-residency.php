81710d137a28ba11e8e7cf807ce72643183ff6fc
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2018-10-08 23:23:55 -0500

    Add residency page template

---
+++ site/web/app/themes/buildcoffee/page-residency.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>Build Coffee Open Call for Artists</title>
+	<?php $base_url = get_template_directory_uri(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base_url ?>/assets/images/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="<?php echo $base_url ?>/assets/images/icons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo $base_url ?>/assets/images/icons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="<?php echo $base_url ?>/assets/images/icons/manifest.json">
+	<link rel="mask-icon" href="<?php echo $base_url ?>/assets/images/icons/safari-pinned-tab.svg" color="#000000">
+	<link rel="shortcut icon" href="<?php echo $base_url ?>/assets/images/icons/favicon.ico">
+	<meta name="msapplication-config" content="<?php echo $base_url ?>/assets/images/icons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	<style type="text/css">
+		html {
+			margin: 0;
+			height: 100%;
+			overflow: hidden;
+		}
+
+		iframe {
+			position: absolute;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			top: 0;
+			border: 0;
+		}
+	</style>
+</head>
+
+<body>
+	<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://buildcoffee.typeform.com/to/lxwOpi"></iframe>
+	<script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
+</body>
+
+</html>
